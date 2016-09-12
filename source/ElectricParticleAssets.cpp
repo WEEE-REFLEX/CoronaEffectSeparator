0c4eee5f90d71d6ed5aedd4443c4ea245db4ecd1
DarioMangoni <dariomangoni@gmail.com>
2016-09-12 12:23:20 -0500

    added material properties

--- source/ElectricParticleAssets.cpp
+++ source/ElectricParticleAssets.cpp
@@ -1,15 +1,16 @@
 #include "ElectricParticleAssets.h"
 
 const std::unordered_map<ElectricParticleAsset::material_type, std::tuple<double, double, std::shared_ptr<ChColorAsset>>> ElectricParticleAsset::default_mat_properties = {
-    { material_type::e_mat_plastic ,{ 1300, 100, std::make_shared<ChColorAsset>(1.00f, 1.00f, 1.00f) } },
-    { material_type::e_mat_metal   ,{ 7500, 100, std::make_shared<ChColorAsset>(0.42f, 0.49f, 0.58f) } },
-    { material_type::e_mat_copper  ,{ 8960, 100, std::make_shared<ChColorAsset>(0.67f, 0.33f, 0.00f) } },
-    { material_type::e_mat_pcb6    ,{ 5000, 100, std::make_shared<ChColorAsset>(0.02f, 0.39f, 0.24f) } },
-    { material_type::e_mat_pcb7    ,{ 5000, 100, std::make_shared<ChColorAsset>(0.58f, 0.02f, 0.02f) } },
-    { material_type::e_mat_pcb8    ,{ 5000, 100, std::make_shared<ChColorAsset>(0.44f, 0.55f, 0.02f) } },
-    { material_type::e_mat_pcb9    ,{ 5000, 100, std::make_shared<ChColorAsset>(0.00f, 0.93f, 0.86f) } },
-    { material_type::e_mat_pcb10   ,{ 5000, 100, std::make_shared<ChColorAsset>(0.92f, 0.99f, 0.00f) } },
-    { material_type::e_mat_other   ,{ 5000, 100, std::make_shared<ChColorAsset>(0.00f, 0.00f, 0.00f) } },
+	// material_type, density, conductivity, color_asset
+    { material_type::e_mat_plastic ,{ 1300, 0, std::make_shared<ChColorAsset>(1.00f, 1.00f, 1.00f) } },
+    { material_type::e_mat_metal   ,{ 7500, 6428000, std::make_shared<ChColorAsset>(0.42f, 0.49f, 0.58f) } },
+    { material_type::e_mat_copper  ,{ 8960, 6428000, std::make_shared<ChColorAsset>(0.67f, 0.33f, 0.00f) } },
+    { material_type::e_mat_pcb6    ,{ 1300, 0, std::make_shared<ChColorAsset>(0.02f, 0.39f, 0.24f) } },
+    { material_type::e_mat_pcb7    ,{ 1300, 0, std::make_shared<ChColorAsset>(0.58f, 0.02f, 0.02f) } },
+    { material_type::e_mat_pcb8    ,{ 1300, 0, std::make_shared<ChColorAsset>(0.44f, 0.55f, 0.02f) } },
+    { material_type::e_mat_pcb9    ,{ 1300, 0, std::make_shared<ChColorAsset>(0.00f, 0.93f, 0.86f) } },
+    { material_type::e_mat_pcb10   ,{ 1300, 0, std::make_shared<ChColorAsset>(0.92f, 0.99f, 0.00f) } },
+    { material_type::e_mat_other   ,{ 3000, 3e6, std::make_shared<ChColorAsset>(0.00f, 0.00f, 0.00f) } },
 };
 
 
